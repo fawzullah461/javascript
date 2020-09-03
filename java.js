2b72d72b5d63b2a4f7807d415e7e4e19435c7b1b
fawzullah461 <fawzullah54@gmail.com>
2020-09-03 12:29:07 +0100

    added new files

---
+++ java.js
@@ -0,0 +1,18 @@
+var today = new Date();
+
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+
+var hrs = today.getHours();
+
+var greet;
+
+if (hrs < 12)
+
+        greet = 'Good Morning';
+    else if (hrs >= 12 && hrs <= 17)
+        greet = 'Good Afternoon';
+    else if (hrs >= 17 && hrs <= 24)
+        greet = 'Good Evening';
+
+alert(date  + " " +  greet + " Dev Mohy, from fawzullah");
+
